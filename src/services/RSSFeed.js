13a7113830d6bfa6051da1eb463df9bdf4880203
Sazzo <stolensoul32@gmail.com>
2020-08-24 20:58:22 -0300

    Debug

--- src/services/RSSFeed.js
+++ src/services/RSSFeed.js
@@ -37,6 +37,7 @@ module.exports.RSSFeed = class RSSFeed {
             anime.save()
             anime.users.forEach(user => {
                 const toSend = client.users.cache.get(user)
+                console.log(`Novo episódio de ${anime._id}! Enviando DM para ${toSend.username}`)
                 toSend.send(`It looks like a new episode of ${anime._id} just came out! Time to watch! ${anime.rss}\nIf you no longer want to receive notifications for this anime, use \`sn!unsubscribe ${anime._id} (or sn!unsub)\`.`)
             });
         }
